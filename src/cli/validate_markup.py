3746febe5c009a561e084ea4565280c85943a09c
Nikolai Makarov <makarov.nikolai20@gmail.com>
2024-08-28 15:48:28 +0300

    some fixed by the linter #1

--- src/cli/validate_markup.py
+++ src/cli/validate_markup.py
@@ -10,10 +10,10 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 
-from src.utils.clap_rules import ClapRulesWrapper
+from src.utils.validation import Validator, plot_accuracy_over_class
 from src.utils.lemmatizer import NatashaBasedLemmatizer
 from src.utils.similarity import NatashaSimilarityWrapper
-from src.utils.validation import Validator, plot_accuracy_over_class
+from src.utils.clap_rules import ClapRulesWrapper
 
 sns.set()
 
@@ -33,9 +33,7 @@ def validate_markup(  # pylint: disable=[too-many-locals]
 ) -> None:
     repository_dir_path: pathlib.Path = pathlib.Path(__file__).parent.parent.parent.resolve()
 
-    embeddings_dump_path: pathlib.Path = (
-        repository_dir_path / './data/additional/embeddings/navec_hudlit_v1_12B_500K_300d_100q.tar'
-    )
+    embeddings_dump_path: pathlib.Path = repository_dir_path / './data/additional/embeddings/navec_hudlit_v1_12B_500K_300d_100q.tar'
     if not os.path.exists(embeddings_dump_path):
         _ = subprocess.run(
             [
@@ -55,18 +53,18 @@ def validate_markup(  # pylint: disable=[too-many-locals]
     markup_table: pd.DataFrame = pd.read_csv(markup_table_path, sep='\t')
     unique_files: list[str] = list(set(markup_table.file_name))
 
-    result_dct: dict[str, list[float] | list[str]] = {}
+    result_dct: dict[str, list[float] | list[str]]  = {}
 
-    result_dct['type'] = []
-    result_dct['mean_accuracy'] = []
+    result_dct["type"] = []
+    result_dct["mean_accuracy"] = []
 
     with open(corrupted_cases_path, 'r', encoding='utf-8') as f:
         corrupted_cases_healer: dict[str, str] = json.load(f)
-
+    
     with open(clap_rules_path, 'r', encoding='utf-8') as f:
         clap_rules_dct = json.load(f)
         clap_rules_wrapper: ClapRulesWrapper = ClapRulesWrapper(lemmatizer, clap_rules_dct)
-
+    
     accuracy_over_label: dict[str, float] = validator.get_accuracy_over_label(
         unique_files,
         markup_table,
@@ -74,14 +72,14 @@ def validate_markup(  # pylint: disable=[too-many-locals]
         similarity_wrapper,
         corrupted_cases_healer,
     )
-
-    result_dct['type'].append('c использованием словаря схлопываний и семантической близости')
-    for label in accuracy_over_label:
+    
+    result_dct["type"].append("c использованием словаря схлопываний и семантической близости")
+    for label, label_accuracy in accuracy_over_label.items():
         if label not in result_dct:
             result_dct[label] = []
-        result_dct[label].append(accuracy_over_label[label])
-    result_dct['mean_accuracy'].append(float(np.mean(list(accuracy_over_label.values()))))
-
+        result_dct[label].append(label_accuracy)
+    result_dct["mean_accuracy"].append(float(np.mean(list(accuracy_over_label.values()))))
+        
     with open(clap_rules_extended_path, 'r', encoding='utf-8') as f:
         clap_rules_dct = json.load(f)
         clap_rules_wrapper = ClapRulesWrapper(lemmatizer, clap_rules_dct)
@@ -92,24 +90,24 @@ def validate_markup(  # pylint: disable=[too-many-locals]
         clap_rules_wrapper,
         similarity_wrapper,
         corrupted_cases_healer,
-    )
+    )    
+    
+    result_dct["type"].append("c использованием расширенного словаря схлопываний и семантической близости")
+    for label, label_accuracy in accuracy_over_label.items():
+        result_dct[label].append(label_accuracy)
+    result_dct["mean_accuracy"].append(float(np.mean(list(accuracy_over_label.values()))))
 
-    result_dct['type'].append('c использованием расширенного словаря схлопываний и семантической близости')
-    for label in accuracy_over_label:
-        result_dct[label].append(accuracy_over_label[label])
-    result_dct['mean_accuracy'].append(float(np.mean(list(accuracy_over_label.values()))))
-
-    tables_dir_path: pathlib.Path = repository_dir_path.joinpath('data/processed/tables')
+    tables_dir_path: pathlib.Path = repository_dir_path.joinpath("data/processed/tables")
     tables_dir_path.mkdir(parents=True, exist_ok=True)
 
     result_df: pd.DataFrame = pd.DataFrame.from_dict(result_dct)
-    result_df.to_csv(tables_dir_path.joinpath('таблица_качества.csv'))
+    result_df.to_csv(tables_dir_path.joinpath("таблица_качества.csv"))
 
-    plots_dir_path: pathlib.Path = repository_dir_path.joinpath('data/processed/plots')
+    plots_dir_path: pathlib.Path = repository_dir_path.joinpath("data/processed/plots")
     plots_dir_path.mkdir(parents=True, exist_ok=True)
     for i in range(result_df.shape[0]):
         plot_accuracy_over_class(result_df.loc[i], True, plots_dir_path)
-
+    
 
 if __name__ == '__main__':
     validate_markup()  # pylint: disable=[no-value-for-parameter]
--- src/utils/validation.py
+++ src/utils/validation.py
@@ -7,9 +7,9 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from tqdm import tqdm
 
-from src.utils.clap_rules import ClapRulesWrapperInterface
 from src.utils.lemmatizer import LemmatizerInterface
 from src.utils.similarity import SimilarityWrapperInterface
+from src.utils.clap_rules import ClapRulesWrapperInterface
 
 
 def plot_accuracy_over_class(
@@ -18,11 +18,9 @@ def plot_accuracy_over_class(
     save_dir_path: pathlib.Path | str | None = None,
 ) -> None:
     columns = list(accuracy_over_label.index)
-    title = (
-        'Accuracy относительно класса \n'
-        + f'{accuracy_over_label.type} \n '
-        + f'Среднее значение accyracy {round(accuracy_over_label.mean_accuracy, 3)}'
-    )
+    title = 'Accuracy относительно класса \n' + \
+            f'{accuracy_over_label.type} \n ' + \
+            f'Среднее значение accyracy {round(accuracy_over_label.mean_accuracy, 3)}'
 
     columns.remove('type')
     columns.remove('mean_accuracy')
@@ -44,7 +42,7 @@ def plot_accuracy_over_class(
     if save_dir_path is not None:
         plt.savefig(
             str(pathlib.Path(save_dir_path).joinpath(f"{accuracy_over_label['type'].replace(' ', '_')}.png")),
-            bbox_inches='tight',
+            bbox_inches="tight",
         )
     plt.close()
 
@@ -56,31 +54,29 @@ class Validator:
     ):
         self._lemmatizer: LemmatizerInterface = lemmatizer
 
-    def get_accuracy_over_label(
+    def get_accuracy_over_label(  # pylint: disable=[too-many-locals]
         self,
         ground_truth: list[str],
         markup_table: pd.DataFrame,
         gesture2homonyms: ClapRulesWrapperInterface,
         similarity_wrapper: SimilarityWrapperInterface,
         corrupted_cases_healer: dict[str, str] | None = None,
-    ) -> dict[str, float]:
+    ) -> dict[str, float]:    
         class_accuracy: dict[str, float] = {}
-
-        for file in tqdm(ground_truth):  # pylint: disable=[too-many-nested-blocks]
+        
+        for file in tqdm(ground_truth): # pylint: disable=[too-many-nested-blocks]
             current_file_markup: pd.DataFrame = markup_table[markup_table.file_name == file]
             true_labels: list[str] = [file[:-4]]
 
             if corrupted_cases_healer is not None:
                 for corrupted_case in corrupted_cases_healer:
                     if corrupted_case in true_labels[0]:
-                        true_labels.append(
-                            true_labels[0].replace(corrupted_case, corrupted_cases_healer[corrupted_case]),
-                        )
+                        true_labels.append(true_labels[0].replace(corrupted_case, corrupted_cases_healer[corrupted_case]))
             true_labels_normalized: list[str] = [self._lemmatizer.lemmatize_text(label) for label in true_labels]
-
+        
             candidates: list[str] = list(current_file_markup['OUTPUT:translation'])
             candidates = [self._lemmatizer.lemmatize_text(candidate) for candidate in candidates]
-
+    
             for candidate in candidates:
                 found: bool = False
                 for true_label_normalized, true_label in zip(true_labels_normalized, true_labels):
@@ -99,7 +95,7 @@ class Validator:
                             break
                     if found:
                         break
-
+            
                 if true_labels[0] not in class_accuracy:
                     class_accuracy[true_labels[0]] = 0
 
